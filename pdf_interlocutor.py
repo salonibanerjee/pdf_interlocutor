feaab4121f883d57b48b4ad7de848563a81c927d
Saloni Banerjee <salonibanerjee97@gmail.com>
2021-02-10 16:51:11 +0530

    Example output Files

--- pdf_interlocutor.py
+++ pdf_interlocutor.py
@@ -1,16 +1,12 @@
 import googletrans
 from googletrans import Translator
-#Extract text from pdf
-import PyPDF2
-#Text to speech
-import pyttsx3
+import PyPDF2   #Extract text from pdf
+import pyttsx3  #Text to speech
 from tkinter import Tk
 from tkinter.filedialog import askopenfilename
-#.txt -> .pdf  :
-from fpdf import FPDF 
+from fpdf import FPDF   #txt -> pdf
 import os
-#to save as mp3:
-from gtts import gTTS
+from gtts import gTTS   #to save as mp3
 import json        #to display choice of language dict
 
 print(json.dumps(googletrans.LANGUAGES, indent = 4))
@@ -24,17 +20,10 @@ Tk().withdraw()                             #Select Japanese PDF
 filelocation = askopenfilename()
 pdfReader = PyPDF2.PdfFileReader(filelocation)
 pages = pdfReader.numPages                  #total no. of pages
-'''
-path = '/Users/Saloni/Downloads/'
-os.chdir("C:")
-os.chdir(path)
-'''
-print("Creating .txt file...", end ="")
+
 f = open("Saloni_new_translation.txt","w")
 f.close()
 
-
-print("\rTranslating inside txt...", end ="")
 for page in range(pages):                   #Translate each foreign language pages and store in .txt
     from_page = pdfReader.getPage(page)
     tt = from_page.extractText() 
@@ -43,7 +32,6 @@ for page in range(pages):                   #Translate each foreign language pag
     f = open("Saloni_new_translation.txt", "a")
     f.write(x.text)       #f.write((translation.text).encode('utf8'))         #print(translation.text,"\n") 
     f.close()
-print("\rTranslated to ",myLang, "!", end =" ")
 
 pdf.add_page()                              #Convert .txt--> .pdf
 pdf.set_font("Arial", size = 15) 
@@ -51,17 +39,14 @@ f = open("Saloni_new_translation.txt", "r")
 for x in f: 
     pdf.cell(200, 10, txt = x, ln = 1, align = 'L') 
 f.close()
-pdf.output("Saloni_new_translation.pdf") 
-print("\rPDF created !\t\t\t", end =" ")
+pdf.output("Saloni_new_translation.pdf")
 
 pdfReader = PyPDF2.PdfFileReader("Saloni_new_translation.pdf")
-pages = pdfReader.numPages                  #total no. of pages
+pages = pdfReader.numPages                  #total no. of pages in translated txt
 speaker = pyttsx3.init()
 voices = speaker.getProperty('voices') 
 speaker.setProperty('voice', voices[0].id)  # changing 0 to 1,2,3.. changes voices
-speaker.say(" hi ...")
-print("\rReading Txt file......", end =" ")
-speaker.say("Starting in 3... 2... 1...")   #Read Saloni_new_translation pdf
+speaker.say("hi ... Starting in 3... 2... 1...")   #Read Saloni_new_translation pdf
 
 f= open('Saloni_new_translation.txt','r')
 for i in f:
@@ -73,13 +58,9 @@ f= open("Saloni_new_translation.txt", "r")
 file = f.read().replace("\n", "   ")
 t=gTTS(text = str(file), lang = myLang, slow = False)
 t.save("Saloni_new_translation.mp3")
-f.close() 
-print("\r .mp3 file Created  !!!", end =" ")
+f.close()
 
 if os.path.exists("Saloni_new_translation.txt"):        #Delete intermediate txt
     os.remove("Saloni_new_translation.txt")
-print("Txt file deleted...", end =" ")
-
 #os.startfile("Saloni_new_translation.mp3")
-print("\rPDF & Audio files Saved at: ",os.getcwd(), end =" ")
-#print("The End :)")
+print("Translated Files Saved at: ",os.getcwd())
